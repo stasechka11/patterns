ea790de8b697616068dc15d693ade3e4ddf72c9e
Anastasia Fokina <45092650+stasechka11@users.noreply.github.com>
2024-02-21 11:22:12 +0400

    refactor DataGenerator

--- src/test/java/ru/netology/delivery/data/DataGenerator.java
+++ src/test/java/ru/netology/delivery/data/DataGenerator.java
@@ -1,42 +1,35 @@
 package ru.netology.delivery.data;
 
 import com.github.javafaker.Faker;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.Value;
-
-import lombok.val;
+import lombok.*;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 import java.util.Random;
 
-@Data
-@NoArgsConstructor
+
 public class DataGenerator {
+    private DataGenerator() {}
 
     public static String generateDate(int shift) {
         // TODO: добавить логику для объявления переменной date и задания её значения, для генерации строки с датой
         // Вы можете использовать класс LocalDate и его методы для получения и форматирования даты
-        String date = LocalDate.now().plusDays(shift).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-        return date;
+        return LocalDate.now().plusDays(shift).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
     }
 
     public static String generateCity(String locale) {
         // TODO: добавить логику для объявления переменной city и задания её значения, генерацию можно выполнить
         // с помощью Faker, либо используя массив валидных городов и класс Random
         Faker faker = new Faker(new Locale(locale));
-        String city = faker.address().cityName();
-        return city;
+        return faker.address().cityName();
     }
 
     public static String generateName(String locale) {
         // TODO: добавить логику для объявления переменной name и задания её значения, для генерации можно
         // использовать Faker
         Faker faker = new Faker(new Locale(locale));
-        String name = faker.name().lastName() + " " + faker.name().firstName();
-        return name;
+        return faker.name().lastName() + " " + faker.name().firstName();
     }
 
   /*  public static String generatePhone(String locale) {
