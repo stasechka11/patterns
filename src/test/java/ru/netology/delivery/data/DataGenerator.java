b21f581862b1d83fb835b5eedd596f354375bd77
Anastasia Fokina <45092650+stasechka11@users.noreply.github.com>
2024-03-01 17:22:00 +0400

    added steps description

--- src/test/java/ru/netology/delivery/data/DataGenerator.java
+++ src/test/java/ru/netology/delivery/data/DataGenerator.java
@@ -1,6 +1,7 @@
 package ru.netology.delivery.data;
 
 import com.github.javafaker.Faker;
+import io.qameta.allure.Step;
 import lombok.*;
 
 import java.time.LocalDate;
@@ -13,33 +14,41 @@ public class DataGenerator {
     private DataGenerator() {
     }
 
+    @Step("Генерация даты доставки")
     public static String generateDate(int shift) {
-        return LocalDate.now().plusDays(shift).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        var deliveryDate = LocalDate.now().plusDays(shift).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        return deliveryDate;
     }
 
+    @Step("Генерация города доставки")
     public static String generateCity() {
         var cities = new String[]{"Майкоп", "Горно-Алтайск", "Уфа", "Улан-Удэ", "Махачкала", "Магас", "Нальчик", "Петразоводск", "Сыктывкар", "Йошкар-Ола",
                 "Саранск", "Якутск", "Владикавказ", "Казань", "Кызыл", "Ижевск", "Абакан", "Грозный", "Чебоксары", "Барнаул", "Чита", "Петропавловск-Камчатский",
                 "Краснодар", "Красноярск", "Пермь", "Ставрополь", "Хабаровск", "Благовещенск", "Архангельск", "Астрахань", "Белгород", "Брянск", "Владимир",
                 "Волгоград", "Вологда", "Воронеж", "Иваново", "Иркутск", "Калининград", "Калуга", "Кемерово", "Киров", "Кострома", "Курск", "Санкт-Петербург",
                 "Липецк", "Магадан", "Москва", "Мурманск", "Нижний Новгород", "Великий Новгород", "Новосибирск", "Омск", "Пенза", "Рязань", "Саратов", "Ярославль"};
-
-        return cities[new Random().nextInt(cities.length)];
+        var city = cities[new Random().nextInt(cities.length)];
+        return city;
     }
 
+    @Step("Генерация фамилии и имени")
     public static String generateName(String locale) {
         Faker faker = new Faker(new Locale(locale));
-        return faker.name().lastName() + " " + faker.name().firstName();
+        var fullName = faker.name().lastName() + " " + faker.name().firstName();
+        return fullName;
     }
 
-      public static String generatePhone(String locale) {
-          Faker faker = new Faker(new Locale(locale));
-          return faker.phoneNumber().phoneNumber();
-      }
+    @Step("Генерация номера телефона")
+    public static String generatePhone(String locale) {
+        Faker faker = new Faker(new Locale(locale));
+        var phoneNumber = faker.phoneNumber().phoneNumber();
+        return phoneNumber;
+    }
+
     public static class Registration {
         private Registration() {
         }
-
+@Step("Генерация данных для ввода")
         public static UserInfo generateUser(String locale) {
             return new UserInfo(generateCity(), generateName(locale), generatePhone(locale));
         }
